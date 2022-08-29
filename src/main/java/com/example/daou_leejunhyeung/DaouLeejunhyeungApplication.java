98528cddf9d018d5460ab1fabe55cf66781f8eb0
james <james@lific.io>
2022-08-29 16:20:09 +0900

    Initial commit

---
+++ src/main/java/com/example/daou_leejunhyeung/DaouLeejunhyeungApplication.java
@@ -0,0 +1,13 @@
+package com.example.daou_leejunhyeung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaouLeejunhyeungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaouLeejunhyeungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daou_leejunhyeung/DaouLeejunhyeungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daou_leejunhyeung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaouLeejunhyeungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
